15cc3853c448cd7df355a7438a66def05dc1322b
nodemon <ooly9138@gmail.com>
2020-07-27 18:53:50 +0900

    connection problem

--- src/db.js
+++ src/db.js
@@ -3,16 +3,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-mongoose.connect(
-  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  }
-);
+mongoose
+  .connect(
+    process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("connection success");
+    console.log(typeof process.env.MONGO_URL_PROD);
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
 
 const db = mongoose.connection;
-console.log("db error", db);
 
 const handleOpen = () => console.log("Connected to DB");
 const handleError = (error) => console.log(`Error on DB Connection:${error}`);
